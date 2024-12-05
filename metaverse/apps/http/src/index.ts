6d474f66f979be7de90c53141c7bb5922d4b6049
NavinVenkatV <vnavinvenkat@gmail.com>
2024-12-05 16:55:58 +0530

    added db via monorepo

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { router } from "./routes";
+import client from "@ui/db/client"
+
 const app = express();
 
-app.use('/api/v1', router);
+app.use('/api/v1',router)
+
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server is running on port 3000")
